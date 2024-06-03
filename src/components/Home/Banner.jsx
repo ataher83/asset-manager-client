3585708c3d89a97bf362c3cd37275ec02e367d75
ataher83 <taher83@gmail.com>
2024-06-03 22:41:26 +0600

    swiper.js installed

--- src/components/Home/Banner.jsx
+++ src/components/Home/Banner.jsx
@@ -7,7 +7,7 @@ const Banner = () => {
     <div className="relative w-full h-96 bg-blue-400 text-white rounded-md">
       <div className="absolute inset-0 flex justify-center items-center space-x-4">
         <div className="bg-black bg-opacity-50 p-6 rounded-lg">
-          <h2 className="text-2xl mb-4">Join as HR Manager</h2>
+          <h2 className="text-2xl mb-4 text-center">For HR Manager</h2>
           <button
             // onClick={() => history.push('/hr-manager')}
             className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
@@ -16,7 +16,7 @@ const Banner = () => {
           </button>
         </div>
         <div className="bg-black bg-opacity-50 p-6 rounded-lg">
-          <h2 className="text-2xl mb-4">Join as Employee</h2>
+          <h2 className="text-2xl mb-4 text-center">For Employee</h2>
           <button
             // onClick={() => history.push('/employee')}
             className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
