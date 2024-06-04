cbea2f520a82a1adf8593b563a80be74cd5e60e8
ataher83 <taher83@gmail.com>
2024-06-04 09:16:18 +0600

    Banner component updated

--- src/components/Home/Banner.jsx
+++ src/components/Home/Banner.jsx
@@ -1,32 +1,59 @@
-// import { useHistory } from 'react-router-dom';
+import React, { useRef, useState } from 'react';
+// Import Swiper React components
+import { Swiper, SwiperSlide } from 'swiper/react';
 
-const Banner = () => {
-//   const history = useHistory();
+// Import Swiper styles
+import 'swiper/css';
+import 'swiper/css/pagination';
+import 'swiper/css/navigation';
 
+import './banner.css';
+
+// import required modules
+import { Autoplay, Pagination, Navigation } from 'swiper/modules';
+
+export default function App() {
   return (
-    <div className="relative w-full h-96 bg-blue-400 text-white rounded-md">
-      <div className="absolute inset-0 flex justify-center items-center space-x-4">
-        <div className="bg-black bg-opacity-50 p-6 rounded-lg">
-          <h2 className="text-2xl mb-4 text-center">For HR Manager</h2>
+    <>
+      <Swiper
+        spaceBetween={30}
+        centeredSlides={true}
+        autoplay={{
+          delay: 2500,
+          disableOnInteraction: false,
+        }}
+        pagination={{
+          clickable: true,
+        }}
+        navigation={true}
+        modules={[Autoplay, Pagination, Navigation]}
+        className="mySwiper"
+      >
+        {/* <SwiperSlide>Slide 1</SwiperSlide>
+        <SwiperSlide>Slide 2</SwiperSlide> */}
+        <SwiperSlide className='relative'>
+          <img className='w-full h-48' src="https://i.ibb.co/ScTPXnh/HR.png" alt="" />
           <button
             // onClick={() => history.push('/hr-manager')}
-            className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+            className="bg-blue-700 hover:bg-blue-500 text-white font-bold py-2 px-4 rounded  absolute"
           >
             Join as HR Manager
           </button>
-        </div>
-        <div className="bg-black bg-opacity-50 p-6 rounded-lg">
-          <h2 className="text-2xl mb-4 text-center">For Employee</h2>
+        </SwiperSlide>
+        <SwiperSlide className='relative'>
+          <img className='w-full h-48' src="https://i.ibb.co/HdZ61wr/employee.png" alt="" />
           <button
-            // onClick={() => history.push('/employee')}
-            className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+            // onClick={() => history.push('/hr-manager')}
+            className="bg-blue-700 hover:bg-blue-500 text-white font-bold py-2 px-4 rounded  absolute"
           >
             Join as Employee
           </button>
-        </div>
-      </div>
-    </div>
-  );
-};
+        </SwiperSlide>
+        {/* <SwiperSlide><img className='w-full h-96' src="https://i.postimg.cc/tTYYTc1m/14.jpg" alt="" /></SwiperSlide>
+        <SwiperSlide><img className='w-full h-96' src="https://i.postimg.cc/pXqgFmmX/15.jpg" alt="" /></SwiperSlide> */}
+        
 
-export default Banner;
+      </Swiper>
+    </>
+  );
+}
