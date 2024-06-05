ff5721d3ed6017c071307b28ab1933d79ecfdfe3
ataher83 <taher83@gmail.com>
2024-06-05 10:28:59 +0600

    dashboard

--- src/layouts/DashboardLayout.jsx
+++ src/layouts/DashboardLayout.jsx
@@ -7,12 +7,7 @@ const DashboardLayout = () => {
     <div className='relative min-h-screen md:flex'>
       <Navbar></Navbar>
       {/* Sidebar */}
-      {/* <div className='mt-52'>
-        <Sidebar />
-      </div> */}
-      <Sidebar />
-      
-
+  
       {/* Outlet --> Dynamic content */}
       <div className='flex-1 md:ml-64'>
         <div className='p-5 mt-32'>
